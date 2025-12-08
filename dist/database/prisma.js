16733dc76ac46d694afadcb4bdd8919f36460b4c
gab Henrique <gabrielbielrique10@gmail.com>
2025-12-08 14:07:40 -0300

    last time to fix this shit

--- dist/database/prisma.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const prisma_1 = require("../generated/prisma");
-const prisma = global.prisma || new prisma_1.PrismaClient();
-if (process.env.NODE_ENV === "development")
-    global.prisma = prisma;
-exports.default = prisma;
--- dist/types/settingType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EMPTY_SETTING = void 0;
-exports.EMPTY_SETTING = {
-    id: undefined,
-    name: '',
-    message: '',
-    jsonvalue: {},
-};
